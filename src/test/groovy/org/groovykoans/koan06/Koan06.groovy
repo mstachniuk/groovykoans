fbebfbc2d2989f6843b22b998f1be9096dc584b3
Marcin Stachniuk <mstachniuk@gmail.com>
2014-07-23 10:06:07 +0200

    Koan06 my solutions

--- src/test/groovy/org/groovykoans/koan06/Koan06.groovy
+++ src/test/groovy/org/groovykoans/koan06/Koan06.groovy
@@ -34,7 +34,12 @@ class Koan06 extends GroovyTestCase {
         // Groovy-fy the above code, using StringBuilder and with() to get the same result in Groovy
         String groovyResult
         // ------------ START EDITING HERE ----------------------
-
+        groovyResult = new StringBuilder().with {
+            append("roses are #FF0000\\n")
+            append("violets are #0000FF\\n")
+            append("all my base\\n")
+            append("are belong to you\\n")
+        }
 
         // ------------ STOP EDITING HERE  ----------------------
         assert groovyResult == javaResult
@@ -50,7 +55,7 @@ class Koan06 extends GroovyTestCase {
         def differentTypes = [1, 'String', "GString", 'a', 'Another string', 0]
         def uniqueTypes = []
         // ------------ START EDITING HERE ----------------------
-
+        uniqueTypes = differentTypes.collect { it.class }.unique()
 
         // ------------ STOP EDITING HERE  ----------------------
         assert uniqueTypes == [Integer, String]
@@ -64,7 +69,15 @@ class Koan06 extends GroovyTestCase {
         // under the src directory
         int count = 0
         // ------------ START EDITING HERE ----------------------
-
+        new File("src").eachFileRecurse {
+            if (it.isFile()) {
+                it.any {
+                    if (it.contains('Lorem')) {
+                        count++;
+                    }
+                }
+            }
+        }
 
         // ------------ STOP EDITING HERE  ----------------------
         assert count == 3
@@ -76,7 +89,11 @@ class Koan06 extends GroovyTestCase {
         // range objects, store all the prime numbers between 200 and 250 in the target variable
         def primesBetween200And250 = []
         // ------------ START EDITING HERE ----------------------
-
+        (200..250).each { x->
+            if ((2..Math.sqrt(x)).every { x % it != 0}) {
+                primesBetween200And250.add x
+            }
+        }
 
         // ------------ STOP EDITING HERE  ----------------------
         assert primesBetween200And250 == [211, 223, 227, 229, 233, 239, 241]
